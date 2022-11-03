c83fcc0bd8d73e21990f48cc80ac7b4cf7fa8882
modengann <99417989+modengann@users.noreply.github.com>
2022-11-03 11:25:51 -0400

    init

---
+++ src/App.java
@@ -0,0 +1,45 @@
+/** YouTube: https://youtu.be/Se4UPB61yag
+ *  Goal: Detect collisions and delete objects based on collision
+ *  Tasks:
+ *  1. Copy methods from Part 4
+ *  2. Add an object of your choosing the start method
+ *  3. Pass your object from #2 into the deleteRain method
+ *  4. Update the deleteRain method to remove rain that collides with your chosen object
+ */
+
+import javadraw.*;
+import java.util.*;
+
+public class App extends Window{
+    ArrayList<Oval> rain;
+    
+    public static void main(String[] args) {
+        Window.open();
+    }
+    //Task 1 - copy method from Task 4
+    //Task 2 - Add an object of your choosing to start. This is the object we will check for collisions with.
+    //         I created a rectangle in my video, choose what you like.
+    //Task 3 - Pass this object as a parameter to deleteRain (same as I did in the video)
+    public void start(){
+       
+        
+    }
+
+    //Task 1 - copy method from Task 4
+
+    public void rainFall(){
+        
+    }
+
+    //Task 1 - copy method from Task 4
+    public void addRain(){
+        
+
+    }
+    //Task 1 - copy method from Task 4
+    //Task 4 - modify deleteRain() to also delete the rain if it touches your object from start
+    public void deleteRain(){
+        
+    }
+
+}
